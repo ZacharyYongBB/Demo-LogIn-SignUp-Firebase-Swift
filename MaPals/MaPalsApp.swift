7a7f7711f8d648743d81dbc1f5f42e66ba4f78ee
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-07 20:13:13 +0800

    Initial Commit

---
+++ MaPals/MaPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaPalsApp.swift
+//  MaPals
+//
+//  Created by Zachary on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MaPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
