23fa08f0d5df4edfbd590cfecf36f9e3ea6e7fcf
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-07 21:11:15 +0800

    Added logo, email & pw input

--- MaPals/ContentView.swift
+++ MaPals/ContentView.swift
@@ -8,10 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        
+        NavigationView{
+            
+            ZStack {
+                
+                Image("background").resizable(resizingMode: .stretch).ignoresSafeArea()
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                    Image("logo")
+                    
+                    Spacer()
+                    
+                    TextField("Enter Email", text: $email)
+                        .frame(width: 250.0, height: 40.0)
+                        .background(Color.gray.opacity(0.8).cornerRadius(10))
+                        .foregroundColor(.red)
+                        .font(.headline)
+                        
+                        
+                   
+                    
+                    TextField("Enter Password", text: $password)
+                        .frame(width: 250.0, height: 40.0)
+                        .background(Color.gray.opacity(0.8).cornerRadius(10))
+                        .foregroundColor(.red)
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                            },
+                           
+                    label: {
+                        Text("Log In")
+                            .padding(.vertical)
+                            .frame(width: 70.0, height: 50.0)
+                            .background(Color.brown.cornerRadius(10))
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        
+                            })
+                    Spacer()
+                    
+                }
+                
+            }
+            .navigationTitle("Log In")
+            
+        }
+        
+        
+        
     }
+    
+    func logIn() {
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
