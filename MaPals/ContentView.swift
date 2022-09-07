3cf3502c268da6a893dd01aa6c1c918fde240589
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-08 07:26:49 +0800

    Users able to logIn and signUp

--- MaPals/ContentView.swift
+++ MaPals/ContentView.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
+
+      
 
 struct ContentView: View {
     
+    
     @State var email: String = ""
     @State var password: String = ""
+    @State var loginSuccess = false
+    @State var showSignUp = false
+    @State var errorText = ""
+
     
-        
     var body: some View {
+        
+        
        
         
         NavigationView{
@@ -30,31 +39,76 @@ struct ContentView: View {
                     
                     Spacer()
                     
+                    
+                    
                     TextField("Enter Email", text: $email)
                         .frame(width: 250.0, height: 40.0)
                         .background(Color.gray.opacity(0.8).cornerRadius(10))
                         .foregroundColor(.red)
                         .font(.headline)
                         
-                        
-                   
-                    
-                    TextField("Enter Password", text: $password)
+            
+                    SecureField("Enter Password", text: $password)
                         .frame(width: 250.0, height: 40.0)
                         .background(Color.gray.opacity(0.8).cornerRadius(10))
                         .foregroundColor(.red)
                         .font(.headline)
                     
+                    
+                    
+                    
                     Spacer()
                     
-                    NavigationLink("Log In", destination: MapScreen())
+                    
+                    
+                    
+                    
+                    Button {
+                                    login()
+                                } label: {
+                                    Text("Sign in")
+                                    .bold()
+                                    .frame(width: 100, height: 50)
+                                    .background(.thinMaterial)
+                                    .cornerRadius(10)
+                                }
+                    
+                    
+                    
+                    
+                    if loginSuccess {
+                                    Text("Login Successfully! ✅")
+                                    .foregroundColor(.green)
+                                } else {
+                                    Text("Not Login Succeessfully Yet! ❌")
+                                    .foregroundColor(.red)
+                                }
+                    
+
+                                Spacer()
+                                
+                                // Button to show the sign up sheet
+                                Button {
+                                    showSignUp.toggle()
+                                } label: {
+                                    Text("Sign Up Here!")
+                                }
+
+                             }
+                             .padding()
+                             .sheet(isPresented: $showSignUp) {
+                                 SignUpView()
+                             }
+                        
+                        
+                    
                     
                     
                     Spacer()
                     
                     
                     
-                }
+                
                 
             }
             .navigationTitle("Log In")
@@ -65,11 +119,20 @@ struct ContentView: View {
         
     }
     
-    func logIn() {
-        
+    func login() {
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if error != nil {
+                    print(error?.localizedDescription ?? "")
+                    loginSuccess = false
+                } else {
+                    print("success")
+                    loginSuccess = true
+                }
+            }
+        }
     }
-    
-}
+
+
 
 
 
